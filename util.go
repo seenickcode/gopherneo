67e8fea3e9af61f0952938aadf4f9e9a14bad119
Nick Manning <nicholas.manning@gmail.com>
2015-03-06 12:45:23 -0500

    More improvements to unescaping for regex in cypher

--- util.go
+++ util.go
@@ -8,12 +8,17 @@ func EscapeStringForCypherRegex(in string) string {
 	r2 := regexp.MustCompile("(\\(|\\))")
 	r3 := regexp.MustCompile("(&)")
 	r4 := regexp.MustCompile("(\\*)")
+	r5 := regexp.MustCompile("(\")")
+	r6 := regexp.MustCompile("(\\+)")
 
 	out := in
 	out = r1.ReplaceAllString(out, "\\\\$1")
 	out = r2.ReplaceAllString(out, "\\\\$1")
 	out = r3.ReplaceAllString(out, "\\\\$1")
 	out = r4.ReplaceAllString(out, "\\\\$1")
+	out = r5.ReplaceAllString(out, "\\$1")
+	out = r6.ReplaceAllString(out, "\\\\$1")
+
 	//fmt.Printf("out: %v\n", out)
 	return out
 }
